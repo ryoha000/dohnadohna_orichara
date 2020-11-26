a7923a4305bd6590c872c089804a7cb59491b3e8
ryoha000 <48485650+ryoha000@users.noreply.github.com>
2020-11-26 16:26:11 +0900

    add test code and image

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,17 @@
 <script lang="ts">
+	import CanvasComponent from "./components/Canvas.svelte";
 	export let name: string;
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<CanvasComponent />
 </main>
+
+<svelte:head>
+	<script src="opencv.js" />
+</svelte:head>
 
 <style>
 	main {
